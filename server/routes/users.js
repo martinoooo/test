e27a6c1192a92bb4b5743792ead716df96691819
martin <897785466@qq.com>
2016-10-16 13:48:46 +0800

    lesson 13

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,30 +2,50 @@
  * Created by meng on 2016/9/30.
  */
 import express from 'express';
-import validateInput from '../shared/validations/signup';
+import commonValidations from '../shared/validations/signup';
 import bcrypt from 'bcrypt';
+import isEmpty from 'lodash/isEmpty'
 
 import User from '../models/user'; 
 
 let router = express.Router();
 
+function validateInput(data,otherValidations){
+	let { errors } = otherValidations(data);
+
+	return Promise.all([
+		User.where({email:data.email}).fetch().then(user => {
+			if(user){errors.email = 'There is user with such eamil';}
+		}),
+		User.where({username:data.username}).fetch().then(user => {
+			if(user){errors.username = 'There is user with such username';}
+		})
+	]).then(() => {
+		return {
+			errors,
+			isValid:isEmpty(errors)
+		}
+	})
+	
+}
+
 router.post('/',(req,res) => {
-  	const {errors,isValid} = validateInput(req.body);
 
-    if(isValid){
-      const {username,password,timezone,email} = req.body;
-      const passworld_digest = bcrypt.hashSync(password,10);
+  	validateInput(req.body,commonValidations).then(({errors,isValid}) => {
+  		if(isValid){
+	      const {username,password,timezone,email} = req.body;
+	      const passworld_digest = bcrypt.hashSync(password,10);
 
-      User.forge({
-      	username,email,timezone,passworld_digest
-      },{ hasTimestamps:true}).save()
-      	.then(user => res.json({success:true}))
-      	.catch(err => res.status(500).json({error:err}))
-      	
-    }else{
-      res.status(400).json(errors);
-    }
-
+	      User.forge({
+	      	username,email,timezone,passworld_digest
+	      },{ hasTimestamps:true}).save()
+	      	.then(user => res.json({success:true}))
+	      	.catch(err => res.status(500).json({error:err}))
+	      	
+	    }else{
+	      res.status(400).json(errors);
+	    }
+  	});
 });
 
 export default router;
